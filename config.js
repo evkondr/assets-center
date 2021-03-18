c59dbf756c5ddb1c7348dfac2e01b82dcd599bde
Evgeniy Kondratyev <evgeniy.kondratyev@danfoss.com>
2021-03-19 01:27:03 +0300

    init mongoose connection to DB

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     port: 3000,
-    dbURI: '',
+    dbURI: 'mongodb+srv://admin:Admin123@cluster0.akizf.mongodb.net/assetsDB'
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose');
 
 const config = require('./config')
 const authRouter = require('./routes/authRouter')
@@ -9,7 +10,21 @@ const app = express()
 //middlewares
 app.use('/api/auth', authRouter)
 app.use('/api/assets', assetsRouter)
-
+async function connectDB(){
+    try{
+        await mongoose.connect(config.dbURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+          });
+          console.log('DB connected')
+    }catch(e){
+        console.log(e)
+    }
+    
+}
+connectDB()
 app.listen(config.port, () =>{
     console.log('app listen on port ' + config.port)
 })
