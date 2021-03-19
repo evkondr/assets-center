0f8e679f2316746571ccc088e72c199bec612ed4
Evgeniy Kondratyev <evgeniy.kondratyev@danfoss.com>
2021-03-20 00:02:28 +0300

    register and login routes modified, installed jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,25 +1,32 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../models/userModel')
-
+// LOGIN CONTROLLER
 exports.login = async (req, res) => {
-    
-    res.status(200).json({"message":"from Login"})
+    const user = {...req.body}
+    const candidate = await Users.findOne({email: user.email}).exec();
+    if(!candidate){
+        return res.status(400).send('Bad Request')
+    }
+    result = await bcrypt.compare(user.password, candidate.password)
+    if(!result){
+        return res.status(400).send('Bad Request')
+    }
+    res.status(200).json({candidate})
 }
-
+// REGISTER CONTROLLER
 exports.register = async (req, res) => {
     try{
-        const user = {...req.body}
-        const candidate = await Users.findOne({email: user.email}).exec();
+        let {email, password} = req.body
+        const candidate = await Users.findOne({email}).exec();
         if(candidate){
             return res.status(400).json({'message': "user with this email already exists"})
         }
-        bcrypt.genSalt(10, function(err, salt) {
-            bcrypt.hash(req.body.password, salt, function(err, hash) {
-                user.password = hash
-            });
-        });
-        await new Users(user).save()
+        const salt = await bcrypt.genSaltSync(10);
+        var hash = await bcrypt.hashSync(password, salt);
+        const user =  new Users({...req.body, password: hash})
+        await user.save()
         res.status(200).json({"user crated": user})
     }catch(e){
         res.status(500).json({"message":e.message})
